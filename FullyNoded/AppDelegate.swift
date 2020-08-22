225a40e77f84d60fe7004d004d46860a02b0cb07
Fonta1n3 <fontainedenton@googlemail.com>
2020-08-22 14:26:06 +0800

    fix: solves some road bumbs for building from source

--- FullyNoded/AppDelegate.swift
+++ FullyNoded/AppDelegate.swift
@@ -15,90 +15,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
-        
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        
-        
-        
-    }
+    func applicationWillResignActive(_ application: UIApplication) {}
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        
-        
-    }
-    
-    
-//    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-//        
-//        addNode(url: "\(url)")
-//        return true
-//        
-//    }
-//    
-//    func addNode(url: String) {
-//        
-//        if let myTabBar = self.window?.rootViewController as? UITabBarController {
-//            
-//            let qc = QuickConnect()
-//            
-//            func getResult() {
-//                
-//                if !qc.errorBool {
-//                    
-//                    print("success adding quick connect")
-//                    
-//                } else {
-//                    
-//                    print("error adding quick connect = \(qc.errorDescription)")
-//                    
-//                }
-//                
-//            }
-//            
-//            qc.addNode(vc: myTabBar,
-//                       url: url,
-//                       completion: getResult)
-//            
-//        } else {
-//            
-//            print("error adding quick connect no access to tabbar")
-//            
-//        }
-//        
-//    }
+    func applicationDidEnterBackground(_ application: UIApplication) {}
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        
         if KeyChain.getData("UnlockPassword") != nil {
-            
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let loginVC = storyboard.instantiateViewController(withIdentifier: "LogIn")
             let topVC = self.window?.rootViewController?.topViewController()
-            
             if topVC!.restorationIdentifier != "LogIn" {
-                
                 topVC!.present(loginVC, animated: true, completion: nil)
-                                
             }
-            
         }
-        
     }
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
+    func applicationDidBecomeActive(_ application: UIApplication) {}
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
         self.saveContext()
     }
 
