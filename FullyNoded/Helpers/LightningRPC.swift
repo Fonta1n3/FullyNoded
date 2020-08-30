9b98a59b09c7cf7a6d20eaa0bb7a18532b295c10
FeedMyTummy <21245786+FeedMyTummy@users.noreply.github.com>
2020-08-30 16:57:05 -0400

    Used a guard let to reduce nesting.

--- FullyNoded/Helpers/LightningRPC.swift
+++ FullyNoded/Helpers/LightningRPC.swift
@@ -19,122 +19,122 @@ class LightningRPC {
         var onionAddress = ""
         
         CoreDataService.retrieveEntity(entityName: .newNodes) { nodes in
-            if nodes != nil {
-                var lightningNode:[String:Any]?
+            guard let nodes = nodes else {
+                completion((id, nil, "error getting nodes from core data"))
                 
-                for node in nodes! {
-                    if let isLightning = node["isLightning"] as? Bool {
-                        if isLightning {
-                            lightningNode = node
-                        }
+                return
+            }
+
+            var lightningNode:[String:Any]?
+            
+            for node in nodes {
+                if let isLightning = node["isLightning"] as? Bool {
+                    if isLightning {
+                        lightningNode = node
                     }
                 }
-                
-                guard lightningNode != nil else {
-                    completion((id, nil, "no lightning node"))
-                    return
-                }
-                
-                func decryptedValue(_ encryptedValue: Data) -> String {
-                    var decryptedValue = ""
-                    Crypto.decryptData(dataToDecrypt: encryptedValue) { decryptedData in
-                        if decryptedData != nil {
-                            decryptedValue = decryptedData!.utf8
-                        }
+            }
+            
+            guard lightningNode != nil else {
+                completion((id, nil, "no lightning node"))
+                return
+            }
+            
+            func decryptedValue(_ encryptedValue: Data) -> String {
+                var decryptedValue = ""
+                Crypto.decryptData(dataToDecrypt: encryptedValue) { decryptedData in
+                    if decryptedData != nil {
+                        decryptedValue = decryptedData!.utf8
                     }
-                    return decryptedValue
                 }
-                
-                let node = NodeStruct(dictionary: lightningNode!)
-                
-                if let encAddress = node.onionAddress {
-                    onionAddress = decryptedValue(encAddress)
-                }
-                if let encUser = node.rpcuser {
-                    rpcusername = decryptedValue(encUser)
-                }
-                if let encPassword = node.rpcpassword {
-                    rpcpassword = decryptedValue(encPassword)
-                }
-                
-                let lightningUrl = "http://\(rpcusername):\(rpcpassword)@\(onionAddress)"
-                guard let url = URL(string: lightningUrl) else {
-                    completion((id, nil, "url error"))
-                    return
-                }
-                
-                var request = URLRequest(url: url)
-                let loginString = String(format: "%@:%@", rpcusername, rpcpassword)
-                let loginData = loginString.data(using: String.Encoding.utf8)!
-                let base64LoginString = loginData.base64EncodedString()
-                if method == .rebalance {
-                    request.timeoutInterval = 120
-                } else if method == .recvmsg {
-                    request.timeoutInterval = 180
-                } else {
-                    request.timeoutInterval = 60
-                }
-                request.addValue("application/json", forHTTPHeaderField: "Accept")
-                request.addValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
-                request.httpMethod = "POST"
-                request.setValue("text/plain", forHTTPHeaderField: "Content-Type")
-                request.httpBody = "{\"jsonrpc\":\"2.0\",\"id\":\"\(id)\",\"method\":\"\(method.rawValue)\",\"params\":[\(param)]}".data(using: .utf8)
-                
-                #if DEBUG
-                print("url = \(url)")
-                print("request: \("{\"jsonrpc\":\"2.0\",\"id\":\"\(id)\",\"method\":\"\(method.rawValue)\",\"params\":[\(param)]}")")
-                #endif
-                
-                let task = torClient.session.dataTask(with: request as URLRequest) { (data, response, error) in
-                    do {
+                return decryptedValue
+            }
+            
+            let node = NodeStruct(dictionary: lightningNode!)
+            
+            if let encAddress = node.onionAddress {
+                onionAddress = decryptedValue(encAddress)
+            }
+            if let encUser = node.rpcuser {
+                rpcusername = decryptedValue(encUser)
+            }
+            if let encPassword = node.rpcpassword {
+                rpcpassword = decryptedValue(encPassword)
+            }
+            
+            let lightningUrl = "http://\(rpcusername):\(rpcpassword)@\(onionAddress)"
+            guard let url = URL(string: lightningUrl) else {
+                completion((id, nil, "url error"))
+                return
+            }
+            
+            var request = URLRequest(url: url)
+            let loginString = String(format: "%@:%@", rpcusername, rpcpassword)
+            let loginData = loginString.data(using: String.Encoding.utf8)!
+            let base64LoginString = loginData.base64EncodedString()
+            if method == .rebalance {
+                request.timeoutInterval = 120
+            } else if method == .recvmsg {
+                request.timeoutInterval = 180
+            } else {
+                request.timeoutInterval = 60
+            }
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            request.addValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+            request.httpMethod = "POST"
+            request.setValue("text/plain", forHTTPHeaderField: "Content-Type")
+            request.httpBody = "{\"jsonrpc\":\"2.0\",\"id\":\"\(id)\",\"method\":\"\(method.rawValue)\",\"params\":[\(param)]}".data(using: .utf8)
+            
+            #if DEBUG
+            print("url = \(url)")
+            print("request: \("{\"jsonrpc\":\"2.0\",\"id\":\"\(id)\",\"method\":\"\(method.rawValue)\",\"params\":[\(param)]}")")
+            #endif
+            
+            let task = torClient.session.dataTask(with: request as URLRequest) { (data, response, error) in
+                do {
+                    
+                    if error != nil {
+                        #if DEBUG
+                        print("error: \(error!.localizedDescription)")
+                        #endif
+                        completion((id, nil, error!.localizedDescription))
                         
-                        if error != nil {
-                            #if DEBUG
-                            print("error: \(error!.localizedDescription)")
-                            #endif
-                            completion((id, nil, error!.localizedDescription))
-                            
-                        } else {
-                            if let urlContent = data {
-                                do {
-                                    let jsonAddressResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableLeaves) as! NSDictionary
+                    } else {
+                        if let urlContent = data {
+                            do {
+                                let jsonAddressResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableLeaves) as! NSDictionary
+                                
+                                #if DEBUG
+                                print("json: \(jsonAddressResult)")
+                                #endif
+                                
+                                if let errorCheck = jsonAddressResult["error"] as? NSDictionary {
+                                    var errorDesc = ""
                                     
-                                    #if DEBUG
-                                    print("json: \(jsonAddressResult)")
-                                    #endif
-                                    
-                                    if let errorCheck = jsonAddressResult["error"] as? NSDictionary {
-                                        var errorDesc = ""
-                                        
-                                        if let errorMessage = errorCheck["message"] as? String {
-                                            errorDesc = errorMessage
-                                            
-                                        } else {
-                                            errorDesc = "Uknown error"
-                                            
-                                        }
-                                        
-                                        completion((id, nil, errorDesc))
+                                    if let errorMessage = errorCheck["message"] as? String {
+                                        errorDesc = errorMessage
                                         
                                     } else {
-                                        completion((id, jsonAddressResult["result"], nil))
+                                        errorDesc = "Uknown error"
                                         
                                     }
                                     
-                                } catch {
-                                    completion((id, nil, "unknown error"))
+                                    completion((id, nil, errorDesc))
+                                    
+                                } else {
+                                    completion((id, jsonAddressResult["result"], nil))
                                     
                                 }
+                                
+                            } catch {
+                                completion((id, nil, "unknown error"))
+                                
                             }
                         }
                     }
                 }
-                task.resume()
-                
-            } else {
-                completion((id, nil, "error getting nodes from core data"))
-                
             }
+            task.resume()
         }
     }
     
