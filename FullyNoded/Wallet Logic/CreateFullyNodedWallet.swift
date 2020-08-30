25211c5286384812128501b876206e66a59d30e6
FeedMyTummy <nick.untethered@icloud.com>
2020-08-29 21:16:15 -0400

    Minor refactor.

--- FullyNoded/Wallet Logic/CreateFullyNodedWallet.swift
+++ FullyNoded/Wallet Logic/CreateFullyNodedWallet.swift
@@ -13,20 +13,11 @@ class Keys {
     
     class func donationAddress() -> String? {
         let randomInt = Int.random(in: 0..<10000)
-        if let hdKey = HDKey("xpub6C1DcRZo4RfYHE5F4yiA2m26wMBLr33qP4xpVdzY1EkHyUdaxwHhAvAUpohwT4ajjd1N9nt7npHrjd3CLqzgfbEYPknaRW8crT2C9xmAy3G") {
-            if let path = BIP32Path("0/\(randomInt)") {
-                do {
-                    let address = try hdKey.derive(path).address(.payToWitnessPubKeyHash)
-                    return address.description
-                } catch {
-                   return nil
-                }
-            } else {
-                return nil
-            }
-        } else {
-            return nil
-        }
+        guard let hdKey = HDKey("xpub6C1DcRZo4RfYHE5F4yiA2m26wMBLr33qP4xpVdzY1EkHyUdaxwHhAvAUpohwT4ajjd1N9nt7npHrjd3CLqzgfbEYPknaRW8crT2C9xmAy3G"),
+              let path = BIP32Path("0/\(randomInt)"),
+              let address = try? hdKey.derive(path).address(.payToWitnessPubKeyHash) else { return nil }
+        
+        return address.description
     }
     
     class func seed() -> String? {
@@ -37,17 +28,15 @@ class Keys {
         if status == errSecSuccess {
             let data = Data(randomBytes)
             let hex = data.hexString
-            if let entropy = BIP39Entropy(hex) {
-                if let mnemonic = BIP39Mnemonic.init(entropy) {
-                    words = mnemonic.description
-                }
+            if let entropy = BIP39Entropy(hex), let mnemonic = BIP39Mnemonic.init(entropy) {
+                words = mnemonic.description
             }
         }
         return words
     }
     
     class func masterKey(words: String, coinType: String, passphrase: String) -> String? {
-        var chain:Network!
+        let chain:Network
         if coinType == "0" {
             chain = .mainnet
         } else {
@@ -56,10 +45,8 @@ class Keys {
         var masterKey:String?
         if let mnmemonic = BIP39Mnemonic(words) {
             let seedHex = mnmemonic.seedHex(passphrase)
-            if let mk = HDKey(seedHex, chain) {
-                if mk.xpriv != nil {
-                    masterKey = mk.xpriv!
-                }
+            if let mk = HDKey(seedHex, chain), let xpriv = mk.xpriv {
+                masterKey = xpriv
             }
         }
         return masterKey
@@ -74,32 +61,18 @@ class Keys {
     }
     
     class func bip84AccountXpub(masterKey: String, coinType: String, account: Int16) -> String? {
-        var xpub:String?
-        if let mk = HDKey(masterKey) {
-            if let path = BIP32Path("m/84'/\(coinType)'/\(account)'") {
-                do {
-                    let accountKey = try mk.derive(path)
-                    xpub = accountKey.xpub
-                } catch {
-                    
-                }
-            }
-        }
-        return xpub
+        guard let mk = HDKey(masterKey),
+              let path = BIP32Path("m/84'/\(coinType)'/\(account)'"),
+              let accountKey = try? mk.derive(path) else { return nil }
+        
+        return accountKey.xpub
     }
     
     class func xpub(path: String, masterKey: String) -> String? {
-        var xpub:String?
-        if let mk = HDKey(masterKey) {
-            if let path = BIP32Path(path) {
-                do {
-                    let accountKey = try mk.derive(path)
-                    xpub = accountKey.xpub
-                } catch {
-                    
-                }
-            }
-        }
-        return xpub
+        guard let mk = HDKey(masterKey),
+              let path = BIP32Path(path),
+              let accountKey = try? mk.derive(path) else { return nil }
+        
+        return accountKey.xpub
     }
 }
