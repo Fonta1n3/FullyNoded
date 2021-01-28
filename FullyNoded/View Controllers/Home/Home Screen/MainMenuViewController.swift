d67f4708917f83aaed1410b100d3d8e2295e90cd
Fonta1n3 <fontainedenton@googlemail.com>
2021-01-28 09:22:54 +0800

    feat: added better error message for "unable to decode response"

--- FullyNoded/View Controllers/Home/Home Screen/MainMenuViewController.swift
+++ FullyNoded/View Controllers/Home/Home Screen/MainMenuViewController.swift
@@ -491,6 +491,9 @@ class MainMenuViewController: UIViewController {
                 } else if errorMessage.contains("timed out") || errorMessage.contains("The Internet connection appears to be offline") {
                     showAlert(vc: self, title: "", message: "Hmmm we are not getting a response from your node, you can try rebooting Tor on your node and force quitting Fully Noded and reopening it, that generally fixes the issue.")
                     
+                } else if errorMessage.contains("Unable to decode the response") {
+                    showAlert(vc: self, title: "", message: "There was an issue... This can mean your rpc credentials are incorrect, double check them. This can also mean your node is busy doing an intense task like rescanning or syncing, if that is the case then just wait a few minutes and try again. As a last resort try rebooting your node and Fully Noded.")
+                    
                 } else {
                     displayAlert(viewController: self, isError: true, message: errorMessage)
                 }
