e877d78927bc4582ef409de66e37670eab843e41
Fonta1n3 <fontainedenton@googlemail.com>
2021-07-06 15:41:48 +0800

    refactor: channel creation

--- FullyNoded/View Controllers/Settings/Node Manager/AddPeerViewController.swift
+++ FullyNoded/View Controllers/Settings/Node Manager/AddPeerViewController.swift
@@ -21,18 +21,14 @@ class AddPeerViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func scanNowAction(_ sender: Any) {
-        if amountField.text != nil {
-            if let _ = Int(amountField.text!) {
-                DispatchQueue.main.async { [unowned vc = self] in
-                    vc.performSegue(withIdentifier: "segueToScannerFromLightningManager", sender: vc)
-                }
-            } else {
-                spinner.removeConnectingView()
-                showAlert(vc: self, title: "Invalid commitment amount", message: "")
-            }
-        } else {
+        guard let amountText = amountField.text, let _ = Int(amountText) else {
             spinner.removeConnectingView()
             showAlert(vc: self, title: "Add a valid commitment amount", message: "")
+            return
+        }
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.performSegue(withIdentifier: "segueToScannerFromLightningManager", sender: self)
         }
     }
     
@@ -48,30 +44,28 @@ class AddPeerViewController: UIViewController, UITextFieldDelegate {
     
     private func addChannel(id: String, ip: String, port: String?) {
         spinner.addConnectingView(vc: self, description: "creating a channel...")
-        if amountField.text != nil {
-            if let int = Int(amountField.text!) {
-                Lightning.connect(amount: int, id: id, ip: ip, port: port ?? "9735") { [weak self] (result, errorMessage) in
-                    if result != nil {
-                        self?.spinner.removeConnectingView()
-                        if let success = result!["commitments_secured"] as? Bool {
-                            if success {
-                                showAlert(vc: self, title: "⚡️⚡️⚡️⚡️⚡️⚡️", message: "Peer connected, channel created, channel started, channel funded and channel commitment secured! That wasn't hard now was it?")
-                            } else {
-                                showAlert(vc: self, title: "Uh oh", message: "So close yet so far! The channel is connected yet we did not seem to get our commitment secured...")
-                            }
-                        }
-                    } else {
-                        self?.spinner.removeConnectingView()
-                        showAlert(vc: self, title: "Ooops something did not go quite right", message: errorMessage ?? "unknown error connecting and funding that peer/channel")
-                    }
-                }
-            } else {
-                spinner.removeConnectingView()
-                showAlert(vc: self, title: "Invalid committment amount", message: "")
-            }
-        } else {
+        
+        guard let amountText = amountField.text, let int = Int(amountText) else {
             spinner.removeConnectingView()
-            showAlert(vc: self, title: "Add a valid committment amount", message: "")
+            showAlert(vc: self, title: "Invalid committment amount", message: "")
+            return
+        }
+        
+        Lightning.connect(amount: int, id: id, ip: ip, port: port ?? "9735") { [weak self] (result, errorMessage) in
+            guard let self = self else { return }
+            
+            self.spinner.removeConnectingView()
+            
+            guard let result = result, let success = result["commitments_secured"] as? Bool else {
+                showAlert(vc: self, title: "Ooops something did not go quite right", message: errorMessage ?? "unknown error connecting and funding that peer/channel")
+                return
+            }
+            
+            if success {
+                showAlert(vc: self, title: "⚡️⚡️⚡️⚡️⚡️⚡️", message: "Peer connected, channel created, channel started, channel funded and channel commitment secured! That wasn't hard now was it?")
+            } else {
+                showAlert(vc: self, title: "Uh oh", message: "So close yet so far! The channel is connected yet we did not seem to get our commitment secured...")
+            }
         }
     }
     
@@ -90,19 +84,25 @@ class AddPeerViewController: UIViewController, UITextFieldDelegate {
         if segue.identifier == "segueToScannerFromLightningManager" {
             if let vc = segue.destination as? QRScannerViewController {
                 vc.isScanningAddress = true
-                vc.onAddressDoneBlock = { url in
-                    if url != nil {
-                        let arr = url!.split(separator: "@")
-                        if arr.count > 0 {
-                            let arr1 = "\(arr[1])".split(separator: ":")
-                            let id = "\(arr[0])"
-                            let ip = "\(arr1[0])"
-                            if arr1.count > 0 {
-                                let port = "\(arr1[1])"
-                                self.addChannel(id: id, ip: ip, port: port)
-                            }
-                        }
-                    }
+                
+                vc.onAddressDoneBlock = { [weak self] url in
+                    guard let self = self else { return }
+                    
+                    guard let url = url else { return }
+                    
+                    let arr = url.split(separator: "@")
+                    
+                    guard arr.count > 0 else { return }
+                    
+                    let arr1 = "\(arr[1])".split(separator: ":")
+                    let id = "\(arr[0])"
+                    let ip = "\(arr1[0])"
+                    
+                    guard arr1.count > 0 else { return }
+                    
+                    let port = "\(arr1[1])"
+                    
+                    self.addChannel(id: id, ip: ip, port: port)
                 }
             }
         }
