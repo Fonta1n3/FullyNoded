513e6bb8d6c8078cd85fd86d4624294026c786cf
Fonta1n3 <fontainedenton@googlemail.com>
2021-09-25 13:34:50 +0800

    fix: uncomment jm code

--- FullyNoded/View Controllers/Home/Home Screen/MainMenuViewController.swift
+++ FullyNoded/View Controllers/Home/Home Screen/MainMenuViewController.swift
@@ -920,14 +920,14 @@ extension MainMenuViewController: OnionManagerDelegate {
         
         timeStamp()
         
-//        let jmPit = JoinMarketPit.sharedInstance
-//        jmPit.connect()
-//        
-//        jmPit.connectedToPit = { connected in
-//            if connected {
-//                jmPit.getOrderBook()
-//            }
-//        }
+        let jmPit = JoinMarketPit.sharedInstance
+        jmPit.connect()
+        
+        jmPit.connectedToPit = { connected in
+            if connected {
+                jmPit.getOrderBook()
+            }
+        }
     }
     
     func torConnDifficulties() {
