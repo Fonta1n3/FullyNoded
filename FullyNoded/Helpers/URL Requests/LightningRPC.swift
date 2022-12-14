a733004a87666c4508d00a4e3e0562cbb7954c7f
Fonta1n3 <fontainedenton@googlemail.com>
2022-12-14 17:40:07 -0500

    refatcor: update for Sparko and nostrnode

--- FullyNoded/Helpers/URL Requests/LightningRPC.swift
+++ FullyNoded/Helpers/URL Requests/LightningRPC.swift
@@ -9,26 +9,51 @@
 import Foundation
 
 class LightningRPC {
-
+    
+    static let sharedInstance = LightningRPC()
     static let torClient = TorClient.sharedInstance
     static var attempts = 0
+    private var isNostr = false
     
-    class func command(id: UUID, method: LIGHTNING_CLI, param: Any, completion: @escaping ((id: UUID, response: Any?, errorDesc: String?)) -> Void) {
-        
-        var rpcusername = ""
-        var rpcpassword = ""
-        var onionAddress = ""
-        
-        CoreDataService.retrieveEntity(entityName: .newNodes) { nodes in
+    private init() {}
+    
+    private func viaNostr(http_body: [String:Any], completion: @escaping ((id: UUID, response: Any?, errorDesc: String?)) -> Void) {
+        MakeRPCCall.sharedInstance.executeClnNostrRpc(http_body: http_body)
+        MakeRPCCall.sharedInstance.onDoneBlock = { nostrResponse in
+            guard let response = nostrResponse.response as? [String:Any] else {
+                completion((UUID(), nil, nostrResponse.errorDesc))
+                return
+            }
+            #if DEBUG
+            print("response: \(response)")
+            #endif
+            guard let message = response["message"] as? String else {
+                completion((UUID(), response, nil))
+                return
+            }
+            completion((UUID(), nil, message))
+        }
+    }
+    
+    func command(id: UUID, method: LIGHTNING_CLI, param: [String:Any]?, completion: @escaping ((id: UUID, response: Any?, errorDesc: String?)) -> Void) {
+        CoreDataService.retrieveEntity(entityName: .newNodes) { [weak self] nodes in
+            guard let self = self else { return }
+            
             guard let nodes = nodes else {
                 completion((id, nil, "error getting nodes from core data"))
                 return
             }
 
             var potentialLightningNode: [String:Any]?
+            var potentialAddress: String?
+            var potentialSparkoKey: String?
             
             for node in nodes {
-                if let isLightning = node["isLightning"] as? Bool, isLightning, let isActive = node["isActive"] as? Bool, isActive {
+                let n = NodeStruct(dictionary: node)
+                if n.isActive && n.isNostr {
+                    self.isNostr = true
+                }
+                if n.isLightning, n.isActive {
                     potentialLightningNode = node
                 }
             }
@@ -41,16 +66,16 @@ class LightningRPC {
             let node = NodeStruct(dictionary: lightningNode)
             
             if let encAddress = node.onionAddress {
-                onionAddress = decryptedValue(encAddress)
-            }
-            if let encUser = node.rpcuser {
-                rpcusername = decryptedValue(encUser)
-            }
-            if let encPassword = node.rpcpassword {
-                rpcpassword = decryptedValue(encPassword)
+                potentialAddress = decryptedValue(encAddress)
             }
             
-            let lightningUrl = "http://\(rpcusername):\(rpcpassword)@\(onionAddress)"
+            if let encSparkoKey = node.rpcpassword {
+                potentialAddress = decryptedValue(encSparkoKey)
+            }
+            
+            guard let sparkoKey = potentialSparkoKey else { return }
+            
+            let lightningUrl = "http://\(potentialAddress ?? "localhost"):9737/rpc"
             
             guard let url = URL(string: lightningUrl) else {
                 completion((id, nil, "url error"))
@@ -58,9 +83,6 @@ class LightningRPC {
             }
             
             var request = URLRequest(url: url)
-            let loginString = String(format: "%@:%@", rpcusername, rpcpassword)
-            let loginData = loginString.data(using: String.Encoding.utf8)!
-            let base64LoginString = loginData.base64EncodedString()
             
             switch method {
             case .rebalance:
@@ -73,60 +95,69 @@ class LightningRPC {
                 break
             }
             
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            request.addValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+            request.addValue(sparkoKey, forHTTPHeaderField: "X-Access")
             request.httpMethod = "POST"
-            request.setValue("text/plain", forHTTPHeaderField: "Content-Type")
-            request.httpBody = "{\"jsonrpc\":\"2.0\",\"id\":\"\(id)\",\"method\":\"\(method.rawValue)\",\"params\":[\(param)]}".data(using: .utf8)
+            let dict:[String:Any] = ["jsonrpc":"2.0","id":id.uuidString,"method":method.rawValue,"params":param ?? [:]]
             
-            #if DEBUG
-            print("url = \(url)")
-            print("request: \("{\"jsonrpc\":\"2.0\",\"id\":\"\(id)\",\"method\":\"\(method.rawValue)\",\"params\":[\(param)]}")")
-            #endif
-            
-            var sesh = URLSession(configuration: .default)
-            
-            if onionAddress.contains("onion") {
-                sesh = self.torClient.session
-            }
-            
-            let task = sesh.dataTask(with: request as URLRequest) { (data, response, error) in
-                guard error == nil else {
+            if self.isNostr {
+                self.viaNostr(http_body: dict, completion: completion)
+            } else {
+                guard let jsonData = try? JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted) else {
                     #if DEBUG
-                    print("error: \(error!.localizedDescription)")
+                    print("converting to jsonData failing...")
                     #endif
-                    
-                    completion((id, nil, error!.localizedDescription))
                     return
                 }
                 
-                guard let urlContent = data else {
-                    completion((id, nil, "Tor client session data is nil"))
-                    return
-                }
-                
-                guard let jsonAddressResult = try? JSONSerialization.jsonObject(with: urlContent,
-                                                                                options: JSONSerialization.ReadingOptions.mutableLeaves) as? NSDictionary else {
-                    completion((id, nil, "Error serializing."))
-                    return
-                }
+                request.httpBody = jsonData
                 
                 #if DEBUG
-                print("json: \(jsonAddressResult)")
+                print("url = \(url)")
+                print("request: \("{\"method\":\"\(method.rawValue)\",\"params\":[\(param)]}")")
                 #endif
                 
-                if let error = jsonAddressResult["error"] as? NSDictionary { // Error path
-                    
-                    if let errorMessage = error["message"] as? String {
-                        completion((id, nil, errorMessage))
-                    } else {
-                        completion((id, nil, "Unknown error"))
+                var sesh = URLSession(configuration: .default)
+                
+    //            if onionAddress.contains("onion") {
+    //                sesh = self.torClient.session
+    //            }
+                
+                let task = sesh.dataTask(with: request as URLRequest) { (data, response, error) in
+                    guard error == nil else {
+                        #if DEBUG
+                        print("error: \(error!.localizedDescription)")
+                        #endif
+                        
+                        completion((id, nil, error!.localizedDescription))
+                        return
                     }
-                } else { // Success path
-                    completion((id, jsonAddressResult["result"], nil))
+                    
+                    guard let urlContent = data else {
+                        completion((id, nil, "Tor client session data is nil"))
+                        return
+                    }
+                    
+                    guard let jsonResult = try? JSONSerialization.jsonObject(with: urlContent, options: .mutableLeaves) as? [String:Any] else {
+                        completion((id, nil, "Error serializing."))
+                        return
+                    }
+                    
+                    #if DEBUG
+                    print("jsonResult: \(jsonResult)")
+                    #endif
+                    
+                    if let error = jsonResult["error"] as? [String:Any] {
+                        if let errorMessage = error["message"] as? String {
+                            completion((id, nil, errorMessage))
+                        } else {
+                            completion((id, nil, "Unknown error"))
+                        }
+                    } else {
+                        completion((id, jsonResult, nil))
+                    }
                 }
+                task.resume()
             }
-            task.resume()
         }
     }
     
