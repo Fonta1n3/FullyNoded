2c5b9dc3fb6ceedeb599ac268cff12443d3750bd
Fonta1n3 <fontainedenton@googlemail.com>
2023-03-28 14:24:37 -0400

    fix: use streammanager for done block to fix cln nostr commands

--- FullyNoded/Helpers/URL Requests/LightningRPC.swift
+++ FullyNoded/Helpers/URL Requests/LightningRPC.swift
@@ -19,18 +19,22 @@ class LightningRPC {
     
     private func viaNostr(http_body: [String:Any], completion: @escaping ((id: UUID, response: Any?, errorDesc: String?)) -> Void) {
         MakeRPCCall.sharedInstance.executeClnNostrRpc(http_body: http_body)
-        MakeRPCCall.sharedInstance.onDoneBlock = { nostrResponse in
+        
+        StreamManager.shared.onDoneBlock = { nostrResponse in
             guard let response = nostrResponse.response as? [String:Any] else {
                 completion((UUID(), nil, nostrResponse.errorDesc))
                 return
             }
+            
             #if DEBUG
             print("response: \(response)")
             #endif
+            
             guard let message = response["message"] as? String else {
                 completion((UUID(), response, nil))
                 return
             }
+            
             completion((UUID(), nil, message))
         }
     }
@@ -43,7 +47,7 @@ class LightningRPC {
                 completion((id, nil, "error getting nodes from core data"))
                 return
             }
-
+            
             var potentialLightningNode: [String:Any]?
             var potentialAddress: String?
             var potentialSparkoKey: String?
@@ -58,54 +62,55 @@ class LightningRPC {
                 }
             }
             
-            guard let lightningNode = potentialLightningNode else {
-                completion((id, nil, "no lightning node"))
-                return
-            }
-            
-            let node = NodeStruct(dictionary: lightningNode)
-            var sesh = URLSession(configuration: .default)
-            
-            if let encAddress = node.onionAddress {
-                potentialAddress = decryptedValue(encAddress)
-                if let add = potentialAddress, add.contains("onion") {
-                    sesh = LightningRPC.torClient.session
-                }
-            }
-            
-            if let encSparkoKey = node.rpcpassword {
-                potentialSparkoKey = decryptedValue(encSparkoKey)
-            }
-            
-            guard let sparkoKey = potentialSparkoKey else { return }
-            
-            let lightningUrl = "http://\(potentialAddress ?? "localhost:9737")/rpc"
-            
-            guard let url = URL(string: lightningUrl) else {
-                completion((id, nil, "url error"))
-                return
-            }
-            
-            var request = URLRequest(url: url)
-            
-            switch method {
-            case .rebalance:
-                request.timeoutInterval = 120
-            case .recvmsg:
-                request.timeoutInterval = 180
-            case .connect:
-                request.timeoutInterval = 90
-            default:
-                break
-            }
-            
-            request.addValue(sparkoKey, forHTTPHeaderField: "X-Access")
-            request.httpMethod = "POST"
             let dict:[String:Any] = ["jsonrpc":"2.0","id":id.uuidString,"method":method.rawValue,"params":param ?? [:]]
             
             if self.isNostr {
                 self.viaNostr(http_body: dict, completion: completion)
             } else {
+                
+                guard let lightningNode = potentialLightningNode else {
+                    completion((id, nil, "no lightning node"))
+                    return
+                }
+                let node = NodeStruct(dictionary: lightningNode)
+                var sesh = URLSession(configuration: .default)
+                
+                if let encAddress = node.onionAddress {
+                    potentialAddress = decryptedValue(encAddress)
+                    if let add = potentialAddress, add.contains("onion") {
+                        sesh = LightningRPC.torClient.session
+                    }
+                }
+                
+                if let encSparkoKey = node.rpcpassword {
+                    potentialSparkoKey = decryptedValue(encSparkoKey)
+                }
+                
+                guard let sparkoKey = potentialSparkoKey else { return }
+                
+                let lightningUrl = "http://\(potentialAddress ?? "localhost:9737")/rpc"
+                
+                guard let url = URL(string: lightningUrl) else {
+                    completion((id, nil, "url error"))
+                    return
+                }
+                
+                var request = URLRequest(url: url)
+                
+                switch method {
+                case .rebalance:
+                    request.timeoutInterval = 120
+                case .recvmsg:
+                    request.timeoutInterval = 180
+                case .connect:
+                    request.timeoutInterval = 90
+                default:
+                    break
+                }
+                
+                request.addValue(sparkoKey, forHTTPHeaderField: "X-Access")
+                request.httpMethod = "POST"
+                
                 guard let jsonData = try? JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted) else {
                     #if DEBUG
                     print("converting to jsonData failing...")
@@ -118,7 +123,7 @@ class LightningRPC {
                 #if DEBUG
                 print("url = \(url)")
                 print("request: \("{\"method\":\"\(method.rawValue)\",\"params\":[\(param)]}")")
-                #endif                
+                #endif
                 
                 let task = sesh.dataTask(with: request as URLRequest) { (data, response, error) in
                     guard error == nil else {
