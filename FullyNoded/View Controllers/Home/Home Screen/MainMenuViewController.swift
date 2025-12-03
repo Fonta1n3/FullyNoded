06f6117de71269ecc1afb47a548f8085ce740791
Fonta1n3 <fontainedenton@googlemail.com>
2025-12-03 09:58:58 +0400

    refactor: remove unused view and temp wallet deletion for testing

--- FullyNoded/View Controllers/Home/Home Screen/MainMenuViewController.swift
+++ FullyNoded/View Controllers/Home/Home Screen/MainMenuViewController.swift
@@ -100,9 +100,6 @@ class MainMenuViewController: UIViewController {
         torProgressLabel.layer.zPosition = 1
         progressView.layer.zPosition = 1
         progressView.setNeedsFocusUpdate()
-        
-        //WalletLogic.shared.taprootBip86Wallet(passphrase: nil, network: .regtest, mnemonic: .init(wordCount: .words12), name: "test_wallet")
-        try? WalletLogic.shared.securelyDeleteWallet(name: "test_wallet")
     }
     
     override func viewDidAppear(_ animated: Bool) {
